58792a0def6769fd576c5d53496661d7cbd606e4
Connor Jakubik <connor.jakubik@gmail.com>
2022-04-07 00:40:39 -0500

    HSV utils
    missing from earlier commit

--- Applications/Connectatron/Connectatron.cpp
+++ Applications/Connectatron/Connectatron.cpp
@@ -47,6 +47,42 @@ const fs::path DevicesPath = "../../../../../Applications/Connectatron/Devices";
 const fs::path ProjectsPath = "../../../../../Applications/Connectatron/Projects";
 #endif
 
+//https://www.codespeedy.com/hsv-to-rgb-in-cpp/
+std::array<float,3> HSVtoRGB(float H, float S, float V) {
+    if (H > 360 || H < 0 || S>100 || S < 0 || V>100 || V < 0) {
+        throw std::runtime_error("The givem HSV values are not in valid range");
+        return {0,0,0};
+    }
+    float s = S / 100;
+    float v = V / 100;
+    float C = s * v;
+    float X = C * (1 - abs(fmod(H / 60.0, 2) - 1));
+    float m = v - C;
+    float r, g, b;
+    if (H >= 0 && H < 60) {
+        r = C, g = X, b = 0;
+    }
+    else if (H >= 60 && H < 120) {
+        r = X, g = C, b = 0;
+    }
+    else if (H >= 120 && H < 180) {
+        r = 0, g = C, b = X;
+    }
+    else if (H >= 180 && H < 240) {
+        r = 0, g = X, b = C;
+    }
+    else if (H >= 240 && H < 300) {
+        r = X, g = 0, b = C;
+    }
+    else {
+        r = C, g = 0, b = X;
+    }
+    float R = (r + m);
+    float G = (g + m);
+    float B = (b + m);
+    return { R,G,B };
+}
+
 
 static inline ImRect ImGui_GetItemRect()
 {
@@ -73,6 +109,9 @@ using ax::Widgets::IconType;
 static ed::EditorContext* m_Editor = nullptr;
 
 static string CurrentProjectName = "my_project.con";
+
+constexpr double HSV_HueFromStringOffset = 90;
+constexpr double HSV_ValFromStringOffset = 0;
 
 //extern "C" __declspec(dllimport) short __stdcall GetAsyncKeyState(int vkey);
 //extern "C" bool Debug_KeyPress(int vkey)
